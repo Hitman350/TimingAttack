eb7cb26732b7f69c212757f98f97511586dde9e5
Hitman350 <aayush.padhy.350@gmail.com>
2025-02-19 11:15:28 +0530

    Added the link to live server in the console.

--- dashboard/app.py
+++ dashboard/app.py
@@ -57,4 +57,6 @@ def handle_stop_stream():
     attack_running = False
 
 if __name__ == "__main__":
-    socketio.run(app, debug=True, host="0.0.0.0", port=5000)
+    port = 5000  # Change if needed
+    print(f"App is running at: http://127.0.0.1:{port}")
+    app.run(host="127.0.0.1", port=port, debug=True)
